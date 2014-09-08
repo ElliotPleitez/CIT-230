86f715469d245d87d40314e32e3343a85caef20b
Elliot J Pleitez <elliot.pleitez@hotmail.com>
2014-09-07 21:14:43 -0700

    Created MyPoint class
    changed getDistance method to accept argument MyPoint for the
    calculation between itself and another

--- Assignments/Ten/Four/MyPoint.java
+++ Assignments/Ten/Four/MyPoint.java
@@ -1,5 +1,42 @@
 package Ten.Four;
 
-public class MyPoint {
+//Author: Elliot J Pleitez, Luisito Espanola
+//Date: 2014-09-07
+//Description: This class defines a point. It is self aware of it's coordinates and can calculate the distance between it and another point.
+//				This class can change it's point.
 
+public class MyPoint {
+	//Fields/Properties
+	private double x, y;
+	
+	//Constructors
+	public MyPoint(){
+		this.x = 0.0;
+		this.y = 0.0;
+	}
+	public MyPoint(double newX, double newY){
+		this.x = newX;
+		this.y = newY;
+	}
+	
+	//Accessor methods (Set & Get)
+	public double getX(){
+		return this.x;
+	}
+	public double getY(){
+		return this.y;
+	}
+	public void setX(double newX){
+		this.x = newX;
+	}
+	public void setY(double newY){
+		this.y = newY;
+	}
+	//Action methods
+	public double getDistance(MyPoint otherPoint){
+		//Distance formula: d = sqrt(square(X2-X1) + square(Y2-Y1))
+		double distance = Math.sqrt(Math.pow(otherPoint.x, this.x) + Math.pow(otherPoint.y, this.y));
+		return distance;
+	}
+	
 }
