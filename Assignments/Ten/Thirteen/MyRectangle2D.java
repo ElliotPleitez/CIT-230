6d8c4093a6feb322e853ca2a50eb86ce16d9e65f
Elliot J Pleitez <elliot.pleitez@hotmail.com>
2014-09-18 20:50:52 -0700

    Added MyPoint
    added to class because it was simpler than offsetting

--- Assignments/Ten/Thirteen/MyRectangle2D.java
+++ Assignments/Ten/Thirteen/MyRectangle2D.java
@@ -1,5 +1,7 @@
 package Ten.Thirteen;
 
+import Ten.Four.MyPoint;
+
 //Author: Elliot J Pleitez, Luisito Espanola
 //Date: 2014-09-16
 //Description: This is a rectangle class, it has a default size, or variable size. It has the ability to calculate it's area, perimeter and whether a point or line is within or crosses itself
@@ -8,9 +10,14 @@ package Ten.Thirteen;
 public class MyRectangle2D {
 	//Fields/Properties
 	private double x, y, length, width;
+	private MyPoint p1, p2, p3, p4;
 	
 	//Constructors
 	public MyRectangle2D(){
+		this.p1 = new MyPoint(0, 0);
+		this.p2 = new MyPoint(0, 1);
+		this.p3 = new MyPoint(1, 1);
+		this.p4 = new MyPoint(1, 0);
 		this.x = 0;
 		this.y = 0;
 		this.length = 1;
@@ -18,6 +25,10 @@ public class MyRectangle2D {
 	}
 	
 	public MyRectangle2D(double x, double y, double length, double width){
+		this.p1 = new MyPoint(x, y);
+		this.p2 = new MyPoint(x, y + width);
+		this.p3 = new MyPoint(x + length, y + width);
+		this.p4 = new MyPoint(x + length, y);
 		this.x = x;
 		this.y = y;
 		this.length = length;
@@ -34,10 +45,18 @@ public class MyRectangle2D {
 	}
 	
 	public void setX(double x){
+		this.p1.setX(x);
+		this.p2.setX(x);
+		this.p3.setX(x + this.length);
+		this.p4.setX(x + this.length);
 		this.x = x;
 	}
 	
 	public void setY(double y){
+		this.p1.setY(y);
+		this.p2.setY(y + this.y);
+		this.p3.setY(y + this.width);
+		this.p4.setY(y);
 		this.y = y;
 	}
 	
@@ -46,6 +65,8 @@ public class MyRectangle2D {
 	}
 	
 	public void setLength(double length){
+		this.p3.setX(this.x + length);
+		this.p4.setX(this.x + length);
 		this.length = length;
 	}
 	
@@ -54,24 +75,31 @@ public class MyRectangle2D {
 	}
 	
 	public void setWidth(double width){
+		this.p2.setY(this.y + width);
+		this.p3.setY(this.y + width);
 		this.width = width;
 	}
 	
 	public double getArea(){
-		return this.calculateArea(this.x, this.y, this.length, this.width);
+		return this.calculateRectangleArea(this.x, this.y, this.length, this.width);
 	}
 	
 	public double getPerimeter(){
 		//Perimeter of a rectangle: p = 2 * (l + w)
-		return Math.abs(2 * ((x - length) + (y - width)));
+		return Math.abs(2 * ((this.x - this.length) + (this.y - this.width)));
 	}
 	
 	//Action methods	
 	public boolean contains(double x, double y){
-		return this.calculateContains(x, y);
+		return this.calculateRectangleContains(x, y);
+	}
+	
+	public boolean contains(MyPoint p){
+		return this.calculateTriangleContains(p);
 	}
 	
 	public boolean contains(MyRectangle2D r){
+		//if(this.contains(r.x, r.y) && this.contains(r.x, r.y + r.width) && this.contains(r.x + r.length, r.y + r.width) && this.contains(r.x + r.length, r.y)){
 		if(this.contains(r.x, r.y) && this.contains(r.x, r.y + r.width) && this.contains(r.x + r.length, r.y + r.width) && this.contains(r.x + r.length, r.y)){
 			return true; //All points are within this
 		}
@@ -112,7 +140,7 @@ public class MyRectangle2D {
 		return false;
 	}
 	
-	private double calculateArea(double x, double y, double length, double width){
+	private double calculateRectangleArea(double x, double y, double length, double width){
 		//Create an array and cross multiply, subtracting then dividing by two to get area
 		//Make so it can grow for polylines grater than four
 		double a, b;
@@ -121,14 +149,21 @@ public class MyRectangle2D {
 		return Math.abs((a - b) / 2);
 	}
 	
-	private boolean calculateContains(double x, double y){
+	private double calculateTriangleArea(MyPoint p1, MyPoint p2, MyPoint p3){
+		//Area of a triangle: a = (h * b) / 2
+		//Area of a triangle: [X1(Y2 - Y3) + X2(Y3 - Y1) + X3(Y1 - Y2)] / 2
+		//Legend: h=height, b=base
+		return Math.abs((p1.getX() * ((p2.getY() - p3.getY())) + p2.getX() * ((p3.getY() - p1.getY())) + p3.getX() * (p1.getY() - p2.getY())) / 2);
+	}
+	
+	private boolean calculateRectangleContains(double x, double y){
 		//Point lie within: if area between r and alternating points around rectangle equal to area of rectangle then r must lie within rectangle
 		//Note: if point lies on line then it does NOT return true
 		double tempArea1, tempArea2, tempArea3, tempArea4;
-		tempArea1 = this.calculateArea(this.x, this.y, x, y);
-		tempArea2 = this.calculateArea(this.x, (this.y + this.width) - y, x, this.width - y);
-		tempArea3 = this.calculateArea((this.x + this.length) - x, (this.width - y) - y, (this.x + this.length) - x, (this.y + this.width) - x);
-		tempArea4 = this.calculateArea((this.x + this.length) - x, y, (this.x + this.length) - x,y - this.y);
+		tempArea1 = this.calculateRectangleArea(this.x, this.y, x, y);
+		tempArea2 = this.calculateRectangleArea(this.x, (this.y + this.width) - y, x, this.width - y);
+		tempArea3 = this.calculateRectangleArea((this.x + this.length) - x, (this.width - y) - y, (this.x + this.length) - x, (this.y + this.width) - x);
+		tempArea4 = this.calculateRectangleArea((this.x + this.length) - x, y, (this.x + this.length) - x,y - this.y);
 		
 		if(tempArea1 + tempArea2 + tempArea3 + tempArea4 == this.getArea()){
 			return true; //x,y lies within this
@@ -136,8 +171,23 @@ public class MyRectangle2D {
 		else{
 			return false; //x,y does not lie within this
 		}
+	}
+	
+	private boolean calculateTriangleContains(MyPoint p){
+		//Point lie within: if area between p and alternating points around triangle equal to area of triangle then p must lie within triangle
+		//Note: if point lies on line then it does NOT return true
+		double tempArea1, tempArea2, tempArea3, tempArea4;
+		tempArea1 = this.calculateTriangleArea(this.p1, this.p2, p);
+		tempArea2 = this.calculateTriangleArea(this.p2, this.p3, p);
+		tempArea3 = this.calculateTriangleArea(this.p3, this.p4, p);
+		tempArea4 = this.calculateTriangleArea(this.p4, this.p1, p);
 		
-		
+		if(tempArea1 + tempArea2 + tempArea3 + tempArea4 == this.getArea()){
+			return true; //p lies within this
+		}
+		else{
+			return false; //p does not lie within this
+		}
 	}
 	
 	//Create a Geometry class to place this in for future use
--- Assignments/Ten/Thirteen/TestMyRectangle2D.java
+++ Assignments/Ten/Thirteen/TestMyRectangle2D.java
@@ -4,16 +4,18 @@ public class TestMyRectangle2D {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		MyRectangle2D rectangle1 = new MyRectangle2D(0, 0, 5, 5);
-//		MyRectangle2D rectangle2 = new MyRectangle2D(3, 3, 10, 10);
-//
-//		System.out.println("Length: " + rectangle1.getLength());
-//		System.out.println("Width: " + rectangle1.getWidth());
-//		System.out.println("Area is: " + rectangle1.getArea());
-//		System.out.println("Perimeter is: " + rectangle1.getPerimeter());
-//		System.out.println("Contains: " + rectangle1.contains(1, 1));
-//		System.out.println("Contains: " + rectangle1.contains(10, 10));
-//		System.out.println("Overlaps: " + rectangle1.overlaps(rectangle2));
+		MyRectangle2D rectangle1 = new MyRectangle2D(0, 0, 5, 5);
+		MyRectangle2D rectangle2 = new MyRectangle2D(3, 3, 10, 10);
+		MyRectangle2D rectangle3 = new MyRectangle2D(5, 5, 7, 7);
+
+//		System.out.println("Gray Length: " + rectangle1.getLength());
+//		System.out.println("Gray Width: " + rectangle1.getWidth());
+//		System.out.println("Gray Area is: " + rectangle1.getArea());
+//		System.out.println("Gray Perimeter is: " + rectangle1.getPerimeter());
+//		System.out.println("Gray Contains Green: " + rectangle1.contains(1, 1));
+//		System.out.println("Gray Contains Orange: " + rectangle1.contains(10, 10));
+		System.out.println("Brown Contains Red: " + rectangle2.contains(rectangle3));
+//		System.out.println("Gray Overlaps Brown: " + rectangle1.overlaps(rectangle2));
 	}
 
 }
