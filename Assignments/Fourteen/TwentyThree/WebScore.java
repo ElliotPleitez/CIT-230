a6a0c64101d64da7df0ff885c842ef9c85a7044f
Elliot J Pleitez <elliot.pleitez@hotmail.com>
2014-10-18 13:25:15 -0700

    initialization & final
    created 14.23 and finished with comments

---
+++ Assignments/Fourteen/TwentyThree/WebScore.java
@@ -0,0 +1,49 @@
+package Fourteen.TwentyThree;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+import Fourteen.Thirteen.Count;
+
+//Author: Elliot J Pleitez
+//Date: 2014-10-18
+//Description: This application searches online for a set of scores. It averages them and displays the total. File has scores separated by the space character
+
+public class WebScore {
+    int count, totalScore;
+
+    //main stub (entry)
+    public static void main(String[] args) {
+        //create an instance of self
+        WebScore webScore = new WebScore();
+        webScore.runMyself();
+    }
+    
+    public void runMyself(){
+        try{
+            //open url stream and begin reading from file
+            URL url = new URL("http://cs.armstrong.edu/liang/data/Scores.txt");
+            Scanner input = new Scanner(url.openStream());
+            //note: could have used the delimeter but this was easier...
+            while(input.hasNextInt()){
+                //while there are integers, add to totalScore and count.
+                this.totalScore += input.nextInt();
+                this.count++;
+            }
+        }
+        catch(IOException e){
+            //return type of error thrown during URL/Scanner class
+            System.out.println(e.getMessage());
+        }
+        if(this.count == 0){
+            //file could either not be found or values were not integers
+            System.out.println("No scores to evaluate, could not find file or integer values.");
+        }
+        else{
+            //echo results to user
+            System.out.println("Total score:   " + this.totalScore);
+            System.out.println("Average score: " + (this.totalScore / this.count));
+        }
+    }
+}
